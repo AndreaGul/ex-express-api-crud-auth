4e76a14d40d12f93754c5123fc6001b71acf6ed3
AndreaGul <andreagulino99@gmail.com>
2024-06-10 17:42:07 +0200

    feat:aggiunta cors policy

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require ("express");
 const postRouter= require("./routers/post");
 const authRouter= require("./routers/auth");
 const app = express();
+const cors = require("cors");
 
 require("dotenv").config();
 const {PORT} = process.env;
 const port = PORT||3000;
+
+app.use(cors());
 
 app.use(express.json());
 
