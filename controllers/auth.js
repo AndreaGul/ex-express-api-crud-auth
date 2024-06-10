00805b349528e4d73edd2ab2cfaddc23a342cf57
AndreaGul <andreagulino99@gmail.com>
2024-06-10 17:09:43 +0200

    feat: controller registration

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,31 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const { hashPassword, comparePassword} = require("../utils/password");
+const generateToken = require("../utils/generateToken")
 
+const registration = async (req,res)=>{
 
-const registration = (req,res)=>{
+    try{
+        const { email,name,password} = req.body;
 
+        const data = {
+            email,
+            name,
+            password: await hashPassword(password),
+        }
+
+        const user = await prisma.user.create({data});
+
+        const token = generateToken({
+            email: user.email,
+            name: user.name
+        });
+
+        res.json({token,data:user});
+    }catch(err){
+        console.error(err);
+        res.status(500).send('Errore del server');
+    }
 }
 
 const login = (req,res)=>{
